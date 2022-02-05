176f3eccf5f9d76de5dc1e7ab65f868f8dc1d027
d7o1d1s0 <d7sarmy@gmail.com>
2022-02-04 21:22:30 -0500

    Again

--- scraper.js
+++ scraper.js
@@ -1,10 +1,13 @@
 const puppeteer = require('puppeteer');
 
+const site = 'http://localhost:3000/'
+const content = '0x8cbd3259c95239f571456bfce2326e121f11d6a6'
+
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-    await page.type('#add', '0x8cbd3259c95239f571456bfce2326e121f11d6a6');
+    await page.goto(site);
+    await page.type('#add', content);
     await page.click('#searchsubmit');
     await page.waitForTimeout(2000);
 
