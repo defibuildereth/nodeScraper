19ff6de1f1fc5a75ea23163e006abd3b77c18abd
d7o1d1s0 <d7sarmy@gmail.com>
2022-02-04 22:27:13 -0500

    general

--- scraper.js
+++ scraper.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
-const site = 'http://localhost:3000/'
-const content = '0x8cbd3259c95239f571456bfce2326e121f11d6a6'
+const site = 'http://localhost:3000/';
+const content = '0x8cbd3259c95239f571456bfce2326e121f11d6a6';
 
 (async () => {
     const browser = await puppeteer.launch();
@@ -13,6 +13,7 @@ const content = '0x8cbd3259c95239f571456bfce2326e121f11d6a6'
 
     const extractedText = await page.$eval('*', (el) => el.innerText)
     console.log(extractedText)
+    // console.log(extractedText.length)
 
     await browser.close();
 })();
